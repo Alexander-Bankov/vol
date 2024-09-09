f1727b6dbe6ed18d560a251c978b4890956548b4
Alexander-Bankov <1bankov20032mail.ru>
2024-09-09 23:07:09 +0400

    create project

---
+++ src/main/java/com/example/webappvolunteer/WebAppVolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappvolunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppVolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppVolunteerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webappvolunteer/WebAppVolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webappvolunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppVolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
